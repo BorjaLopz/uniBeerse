d1e6b6dfc83e879208cdf6326e57ea5e7c9a4f7c
Borja <lopzdiazborja@gmail.com>
2023-08-29 20:29:44 +0200

    Añadimos mas funcionalidades

--- frontend/src/components/BeerStyleComponent/BeerStyleComponent.jsx
+++ frontend/src/components/BeerStyleComponent/BeerStyleComponent.jsx
@@ -4,26 +4,24 @@ import "./style.css";
 import { Link } from "react-router-dom";
 
 function BeerStyleComponent({ b }) {
-  console.log("b");
-  console.log(b);
   return (
     <>
-      <Link key={b?.id} to={`/beer/${b?.id}`} className="currentBeer">
+      <Link key={b?.id} to={`/beer/${b?.id}`} className="currentBeer_style">
         <div id="beer_card_style">
           <div id="beer_brand_name">
             <h2 id="beer_brand">{b?.brand}</h2>
             <h2 id="beer_name">{b?.name}</h2>
           </div>
           {b?.img_file === "" ? (
-            <>
+            <div id="beer_icon_style">
               <BeerIcon />
-            </>
+            </div>
           ) : (
             <>
               <img
                 src={b?.img_file}
                 alt={`Imagen de ${b?.name}`}
-                id="beer_image"
+                id="beer_image_style"
               />
             </>
           )}
